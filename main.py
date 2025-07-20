c677ef043e1775907812401c18889509e8552085
Jorge Guzmán <jorge.guzman.arciniegas@gmail.com>
2025-07-19 20:47:56 -0500

    Improved the main.py code

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ from utils.json_management import load_json
 from utils.logger_config import logger
 from decouple import config
 
-def main():
+def main(mongo_uri, mongo_db, mongo_collection):
     try:
-        #logger.info("Loading settings and creating storage folder")
         settings_dict = load_json('settings.json')
         logger.info("Configuring Scrapers...")
         scrapers = []
-
-        mongo_uri = f'mongodb://{config("MONGO_INITDB_ROOT_USERNAME")}:{config("MONGO_INITDB_ROOT_PASSWORD")}@localhost:27017/?authSource=admin'
-        mongo_db = config("MONGO_DB")
-        mongo_collection = config("MONGO_COLLECTION")
 
         for website in settings_dict['news_websites']:
             scraper = NewsScraper(
@@ -20,6 +15,7 @@ def main():
                 mongo_uri,
                 mongo_db,
                 mongo_collection)
+            
             scrapers.append(scraper)
 
         count = 0
@@ -35,6 +31,10 @@ def main():
         logger.critical(f"Unexpected error: {e}")
 
 if __name__ == '__main__':
+    mongo_uri = f'mongodb://{config("MONGO_INITDB_ROOT_USERNAME")}:{config("MONGO_INITDB_ROOT_PASSWORD")}@localhost:27017/?authSource=admin'
+    mongo_db = config("MONGO_DB")
+    mongo_collection = config("MONGO_COLLECTION")
+
     logger.info("Starting Scraping Process...")
     main()
     logger.info("Scraping Process Ended")
